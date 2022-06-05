c4dae951de4c4dc28456a92941d822af8002b98b
un-earthly <vijayalamin@gmail.com>
2022-06-05 04:21:26 -0700

    banner

--- Components/Homebanner.js
+++ Components/Homebanner.js
@@ -1,7 +1,34 @@
 import React from 'react'
-
+import bannerStyles from "../styles/homebanner.module.css"
+import { Carousel } from 'react-responsive-carousel';
+import Image from 'next/image';
 export default function Homebanner() {
+    const src = ""
     return (
-        <div>Homebanner</div>
+        <div className={bannerStyles.banner__container}>
+            <div className={bannerStyles.banner__typography}>
+                <h1>Lorem ipsum dolor sit amet consectetur, sa atque.</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi soluta dolore quae tempora asperiores maxime magni explicabo, accusantium inventore placeat unde quo illo ea perferendis illum commodi error porro ut!</p>
+                <button className={bannerStyles.banner__btn}>Grab Your's</button>
+            </div>
+            <div className={bannerStyles.banner__images}>
+                <Carousel showThumbs={false} infiniteLoop={true} showStatus={false} autoPlay={true}>
+                    <div>
+                        <Image src="https://cdn.shopify.com/s/files/1/0606/7876/6786/products/rollerblade-rb-cruiser-inline-skates-black-yellow-22_720x.jpg?v=1654279615" width={500}
+                            height={500} />
+                    </div>
+                    <div>
+                        <Image src="https://cdn.shopify.com/s/files/1/0606/7876/6786/products/609732_609732_1_720x.jpg?v=1650912719" width={500}
+                            height={500} />
+
+                    </div>
+                    <div>
+                        <Image src="https://cdn.shopify.com/s/files/1/0606/7876/6786/products/432474_432474_720x.jpg?v=1654279461" width={500}
+                            height={500} />
+
+                    </div>
+                </Carousel>
+            </div>
+        </div>
     )
 }
