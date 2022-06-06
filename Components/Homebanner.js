b579ea8254e019ac05a1f7b3b83ed46c8a12c92a
un-earthly <vijayalamin@gmail.com>
2022-06-06 04:18:58 -0700

    hombanner ready

--- Components/Homebanner.js
+++ Components/Homebanner.js
@@ -2,17 +2,17 @@ import React from 'react'
 import bannerStyles from "../styles/homebanner.module.css"
 import { Carousel } from 'react-responsive-carousel';
 import Image from 'next/image';
+import Btn from '../Utalities/Btn';
 export default function Homebanner() {
-    const src = ""
     return (
         <div className={bannerStyles.banner__container}>
             <div className={bannerStyles.banner__typography}>
-                <h1>Lorem ipsum dolor sit amet consectetur, sa atque.</h1>
+                <h1>Get The <span className='highlight'>Best Skate</span> For You.</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi soluta dolore quae tempora asperiores maxime magni explicabo, accusantium inventore placeat unde quo illo ea perferendis illum commodi error porro ut!</p>
-                <button className={bannerStyles.banner__btn}>Grab Your's</button>
+                <Btn>Grab Yours</Btn>
             </div>
             <div className={bannerStyles.banner__images}>
-                <Carousel showThumbs={false} infiniteLoop={true} showStatus={false} autoPlay={true}>
+                <Carousel showThumbs={false} infiniteLoop={true} showStatus={false} autoPlay={true} useKeyboardArrows={true} showArrows={false}>
                     <div>
                         <Image src="https://cdn.shopify.com/s/files/1/0606/7876/6786/products/rollerblade-rb-cruiser-inline-skates-black-yellow-22_720x.jpg?v=1654279615" width={500}
                             height={500} />
---
+++ Utalities/Btn.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function Btn({children}) {
+    return <button className="btn">{children}</button>
+}
